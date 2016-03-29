ccc4031be9e709710d7d96697c177050d0b0168d
Amos Omondi <amos.omondi@andela.com>
2016-03-29 19:05:41 +0300

    (Chore) Start creation of modelsfor API

--- bucketlist/api/models.py
+++ bucketlist/api/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class BucketList(models.Model):
+    pass
+
+class BucketListItem(models.model):
+    pass
