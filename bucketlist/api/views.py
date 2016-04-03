6de8784f7e5d0d60df903708b14d844d46948a24
Amos Omondi <amos.omondi@andela.com>
2016-04-03 17:38:00 +0300

    (Feature) Write views for actions on bucketlists

--- bucketlist/api/views.py
+++ bucketlist/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from api.models import BucketList, BucketListItem
+from api.serializers import BucketlistSerializer, BucketlistItemSerializer
+from rest_framework import generics
+from rest_framework import permissions
+
 
 # Create your views here.
+class BucketListView(generics.ListCreateAPIView):
+    # get, post '/bucketlists/'
+    queryset = BucketList.objects.all()
+    serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        # set owner of bucketlist to be current logged in user
+        # when creating
+        serializer.save(user=self.request.user)
+
+
+class BucketListDetailView(generics.RetrieveUpdateDestroyAPIView):
+    # get, put, delete '/bucketlists/<id>'
+    queryset = BucketList.objects.all()
+    serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class BucketlistItemCreateView(generics.CreateAPIView):
+    # post '/bucketlists/<id>/items/'
+    pass
+
+
+class BucketlistItemActionView(generics.UpdateAPIView, generics.DestroyAPIView):
+    # put, delete '/bucketlists/<id>/items/<item_id>'
+    pass
