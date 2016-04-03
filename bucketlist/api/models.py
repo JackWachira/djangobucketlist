0bab7841ccfd66ceab63455c55636db4ab10d5e5
Amos Omondi <amos.omondi@andela.com>
2016-04-03 17:36:17 +0300

    (Fix) Change how dates are created to use inbuilt Django methods

--- bucketlist/api/models.py
+++ bucketlist/api/models.py
@@ -9,10 +9,10 @@ from django.db import models
 class BucketList(models.Model):
     name = models.CharField(blank=False, unique=True, max_length=255,
                             default="bucket")
-    date_created = models.DateField(default=datetime.now, editable=False)
-    date_updated = models.DateField(default=datetime.now)
+    date_created = models.DateField(auto_now_add=True, editable=False)
+    date_updated = models.DateField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE,
-                             default=1)  # like created_by
+                             default=1, editable=False)  # like created_by
 
     def __unicode__(self):
         return u'%s' % self.name
@@ -21,8 +21,8 @@ class BucketList(models.Model):
 class BucketListItem(models.Model):
     name = models.CharField(blank=False, unique=True, max_length=255,
                             default="item")
-    date_created = models.DateField(default=datetime.now, editable=False)
-    date_updated = models.DateField(default=datetime.now)
+    date_created = models.DateField(auto_now_add=True, editable=False)
+    date_updated = models.DateField(auto_now=True)
     done = models.BooleanField(default=False)
     bucketlist = models.ForeignKey(BucketList, on_delete=models.CASCADE,
                                    related_name="items", default=1)
