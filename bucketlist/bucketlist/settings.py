52d3e31d0dbee295725035139c1dce650916f311
Amos Omondi <amos.omondi@andela.com>
2016-04-10 00:42:19 +0300

    (Chore) Miscellaneous fixes to allow templates to render properly

--- bucketlist/bucketlist/settings.py
+++ bucketlist/bucketlist/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     'api',
     'django_nose',
+    'frontend'
 ]
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
@@ -148,3 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
