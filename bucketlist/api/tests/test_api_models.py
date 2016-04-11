35b23b84ab40f1b775f5b1d1d921345a7c48e8f4
Amos Omondi <amos.omondi@andela.com>
2016-04-11 22:29:27 +0300

    (Chore) Add tests

--- bucketlist/api/tests/test_api_models.py
+++ bucketlist/api/tests/test_api_models.py
@@ -2,19 +2,40 @@ from api.models import BucketList, BucketListItem
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-user = User.objects.create(username="xyzj", password="12345")
-bucketlist = BucketList.objects.create(name="Life Goals", user=user)
-item = BucketListItem.objects.create(name="Climb Mt. Everest",
-                                     bucketlist=bucketlist)
+
+class UserModelTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="xyzjv", password="12345")
+        self.assertEqual(str(user), 'xyzjv')
 
 
-class BucketListTestCase(TestCase):
+class BucketListModelTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="xyzjv", password="12345")
+        bucketlist = BucketList.objects.create(name="Life Goals", user=user)
+        item = BucketListItem.objects.create(name="Climb Mt. Everest",
+                                             bucketlist=bucketlist)
+
     def test_bucketlist_model(self):
-        self.assertEqual(bucketlist.name, 'Life Goals')
-        self.asserEqual(bucketlist.id, 1)
+        user = User.objects.get(username="xyzjv")
+        bucketlist = BucketList.objects.get(id=1)
+        self.assertEqual(str(bucketlist), 'Life Goals')
+        self.assertEqual(bucketlist.id, 1)
+        self.assertEqual(bucketlist.user, user)
+        self.assertEqual(BucketList.objects.count(), 1)
+
+
+class BucketListItemModelTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="xyzjv", password="12345")
+        bucketlist = BucketList.objects.create(name="Life Goals", user=user)
+        item = BucketListItem.objects.create(name="Climb Mt. Everest",
+                                             bucketlist=bucketlist)
 
     def test_bucketlist_item_model(self):
-        self.assertEqual(item.name, "Climb Mt. Everest")
+        bucketlist = BucketList.objects.get(id=1)
+        item = BucketListItem.objects.get(id=1)
+        self.assertEqual(str(item), "Climb Mt. Everest")
         self.assertEqual(item.id, 1)
         self.assertEqual(item.bucketlist, bucketlist)
         self.assertEqual(BucketListItem.objects.count(), 1)
--- bucketlist/api/tests/test_api_views.py
+++ bucketlist/api/tests/test_api_views.py
@@ -7,24 +7,77 @@ from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 
+client = APIClient()
 
-class APIAccess(APITestCase):
-    # def setUp:
-    #     test_user = User.objects.create(username="amos", first_name="Amos",
-    #                                     last_name="Omondi",
-    #                                     email="amos.omondi@andela.com",
-    #                                     password="12345")
+
+class APIAccessTestCase(APITestCase):
+    def setUp(self):
+        client.post('/api/auth/register/',
+                    {
+                        'username': 'amos',
+                        'email': 'amos.omondi@andela.com',
+                        'password': '12345',
+                    },
+                    format='json')
 
     def test_unauthorized_access_is_denied(self):
-        response = self.client.get('api/bucketlists/')
+        response = self.client.get('/api/bucketlists/')
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    # def test_create_account(self):
-    #     pass
+    def test_authorization_returns_a_token(self):
+        response = client.post('/api/auth/login/',
+                               {
+                                    'username': 'amos',
+                                    'password': '12345',
+                               },
+                               format='json')
+
+        self.assertIn("token", response.data)
+
+
+class APIBucketListTestCase(APITestCase):
+    def setUp(self):
+        client.post('/api/auth/register/',
+                    {
+                        'username': 'amos',
+                        'email': 'amos.omondi@andela.com',
+                        'password': '12345',
+                    },
+                    format='json')
+
+    def test_bucketlist_creation(self):
+        login_response = client.post('/api/auth/login/',
+                                     {
+                                        'username': 'amos',
+                                        'password': '12345',
+                                     },
+                                     format='json')
+
+        token = login_response.data["token"]
+
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+
+        creation_response = client.post('/api/bucketlists/',
+                                        {
+                                            'name': 'Books I want to read',
+                                        },
+                                        format='json')
+
+        self.assertEqual(creation_response.status_code, 201)
+
+    # def test_bucketlist_querying(self):
+    #     login_response = client.post('/api/auth/login/',
+    #                                  {
+    #                                     'username': 'amos',
+    #                                     'password': '12345',
+    #                                  },
+    #                                  format='json')
     #
-    # def test_authorization_returns_a_token(self):
-    #     pass
-    # token = Token.objects.get(user__username='lauren')
-    # client = APIClient()
-    # client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+    #     token = login_response.data["token"]
+    #
+    #     client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+    #
+    #     bucketlist = client.get('/api/bucketlists/', format='json')
+    #
+    #     self.assertTrue(bucketlist.id, 1)
