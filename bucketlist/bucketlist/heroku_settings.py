df4db9405178c20b6fa630d86a87af9fb9972a8e
Amos Omondi <amos.omondi@andela.com>
2016-04-21 18:36:10 +0300

    (Fix) Fix heroku settings

--- bucketlist/bucketlist/heroku_settings.py
+++ bucketlist/bucketlist/heroku_settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'bucketlist.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
 
 # Password validation
